16e80e531a88a3015c26c259a659f74c997f491a
fdiazcaballero <fdiazcaballero@gmail.com>
2016-04-07 09:52:51 +0200

    blog

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -7,18 +7,30 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = [
+        // When you install a third-party bundle or create a new bundle in your
+        // application, you must add it in the following array to register it
+        // in the application. Otherwise, the bundle won't be enabled and you
+        // won't be able to use it.
+        $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
+            new CodeExplorerBundle\CodeExplorerBundle(),
             new AppBundle\AppBundle(),
-        ];
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+        );
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        // Some bundles are only used while developing the application or during
+        // the unit and functional tests. Therefore, they are only registered
+        // when the application runs in 'dev' or 'test' environments. This allows
+        // to increase application performance in the production environment.
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -28,23 +40,8 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
-    public function getRootDir()
-    {
-        return __DIR__;
-    }
-
-    public function getCacheDir()
-    {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
-    }
-
-    public function getLogDir()
-    {
-        return dirname(__DIR__).'/var/logs';
-    }
-
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
